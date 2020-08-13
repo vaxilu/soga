87fed9144213980ca09c15a88274ecdae19e9730
sprov <47310637+sprov065@users.noreply.github.com>
2020-08-14 07:35:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -89,13 +89,30 @@ install_soga() {
     if [[ -e /usr/local/soga/ ]]; then
         rm /usr/local/soga/ -rf
     fi
-    last_version=$(curl -Ls "https://api.github.com/repos/sprov065/soga/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-    echo -e "检测到 soga 最新版本：${last_version}，开始安装"
-    wget -N --no-check-certificate -O /usr/local/soga.tar.gz https://github.com/sprov065/soga/releases/download/${last_version}/soga-linux64.tar.gz
-    if [[ $? -ne 0 ]]; then
-        echo -e "${red}下载 soga 失败，请确保你的服务器能够下载 Github 的文件，如果多次安装失败，请参考手动安装教程${plain}"
-        exit 1
+
+    if  [ ! -n "$1" ] ;then
+        last_version=$(curl -Ls "https://api.github.com/repos/sprov065/soga/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        echo -e "检测到 soga 最新版本：${last_version}，开始安装"
+        wget -N --no-check-certificate -O /usr/local/soga.tar.gz https://github.com/sprov065/soga/releases/download/${last_version}/soga-linux64.tar.gz
+        if [[ $? -ne 0 ]]; then
+            echo -e "${red}下载 soga 失败，请确保你的服务器能够下载 Github 的文件，如果多次安装失败，请参考手动安装教程${plain}"
+            exit 1
+        fi
+    else
+        url=$(curl -Ls "https://api.github.com/repos/sprov065/soga/releases" | grep -o 'https.*soga-linux64\.tar\.gz' | grep "/$1/")
+        if  [ ! -n "$url" ] ;then
+            echo -e "${red}找不到版本 ${plain} ${green}$1${plain}，请确保此版本存在"
+            exit 1
+        fi
+        last_version=$1
+        echo -e "开始安装 soga v$1"
+        wget -N --no-check-certificate -O /usr/local/soga.tar.gz ${url}
+        if [[ $? -ne 0 ]]; then
+            echo -e "${red}下载 soga v$1 失败，请确保你的服务器能够下载 Github 的文件，如果多次安装失败，请参考手动安装教程${plain}"
+            exit 1
+        fi
     fi
+
     tar zxvf soga.tar.gz
     rm soga.tar.gz -f
     cd soga
@@ -136,6 +153,7 @@ install_soga() {
     echo "soga disable      - 取消 soga 开机自启"
     echo "soga log          - 查看 soga 日志"
     echo "soga update       - 更新 soga"
+    echo "soga update x.x.x - 更新 soga 指定版本"
     echo "soga install      - 安装 soga"
     echo "soga uninstall    - 卸载 soga"
     echo "soga version      - 查看 soga 版本"
@@ -145,4 +163,4 @@ install_soga() {
 echo -e "${green}开始安装${plain}"
 install_base
 install_acme
-install_soga
+install_soga $1
